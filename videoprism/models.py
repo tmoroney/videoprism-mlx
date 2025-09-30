431290b059c6236a903e2795ebb7b0bf9228f529
Ting Liu <liuti@google.com>
2025-09-29 23:26:05 -0700

    Support string argument for `fprop_dtype`.
    PiperOrigin-RevId: 813107918

--- videoprism/models.py
+++ videoprism/models.py
@@ -266,7 +266,7 @@ def get_model(
     model_name: str | None,
     model_fn: Callable[[], nn.Module] | None = None,
     models: Mapping[str, Callable[[], nn.Module]] | None = None,
-    fprop_dtype: jnp.dtype | None = None,
+    fprop_dtype: jnp.dtype | str | None = None,
 ):
   """Returns VideoPrism model with the given name.
 
